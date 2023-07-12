36aaf4bcf4a9441ffb79c3c9d9ef833c7f431a4f
Vlad Moroz <hi@vladmoroz.com>
2023-07-12 18:00:13 +0200

    Music app content

--- components/themes/ExampleMusicApp.tsx
+++ components/themes/ExampleMusicApp.tsx
@@ -7,7 +7,6 @@ import {
   Separator,
   Theme,
   Slider,
-  Select,
   Switch,
   Grid,
   IconButton,
@@ -43,7 +42,7 @@ export const ExampleMusicApp = () => (
         </Box>
 
         <Flex direction="column" gap="4">
-          {songs.slice(0, 5).map((song) => (
+          {songsQueue.slice(0, 5).map((song) => (
             <Flex align="center" gap="3" key={song.name}>
               <Box asChild width="8" height="8">
                 <img
@@ -281,7 +280,7 @@ export const ExampleMusicApp = () => (
         </Box>
 
         <Flex direction="column" gap="4">
-          {songs.slice(3).map((song, i) => (
+          {songsHistory.map((song, i) => (
             <Flex align="center" gap="3" key={song.name}>
               <Box asChild width="8" height="8">
                 <img
@@ -416,13 +415,13 @@ export const ExampleMusicApp = () => (
         </Box>
 
         <Grid columns="5" gap="5" mb="7">
-          {songs.slice(2, 7).map((song) => (
+          {albumsFavorites.map((album) => (
             <AlbumCard
-              album={song.album}
-              artist={song.artist}
-              cover={song.cover}
-              color={song.color}
-              key={song.name}
+              title={album.name}
+              caption={album.artist}
+              cover={album.cover}
+              color={album.color}
+              key={album.name}
             />
           ))}
         </Grid>
@@ -440,13 +439,13 @@ export const ExampleMusicApp = () => (
         </Box>
 
         <Grid columns="5" gap="5" mb="7">
-          {songs.slice(0, 5).map((song) => (
+          {playlistsForYou.slice(0, 5).map((playlist) => (
             <AlbumCard
-              album={song.album}
-              artist={song.artist}
-              cover={song.cover}
-              color={song.color}
-              key={song.name}
+              title={playlist.title}
+              caption={playlist.caption}
+              cover={playlist.cover}
+              color={playlist.color}
+              key={playlist.title}
             />
           ))}
         </Grid>
@@ -454,7 +453,7 @@ export const ExampleMusicApp = () => (
         <Box mb="4">
           <Flex align="baseline" justify="between">
             <Heading as="h2" size="6">
-              Your friends liked
+              Your friends listen
             </Heading>
 
             <Link size="2" weight="medium">
@@ -464,13 +463,13 @@ export const ExampleMusicApp = () => (
         </Box>
 
         <Grid columns="5" gap="5">
-          {songs.slice(8, 13).map((song) => (
+          {albumsFriendsListen.map((album) => (
             <AlbumCard
-              album={song.album}
-              artist={song.artist}
-              cover={song.cover}
-              color={song.color}
-              key={song.name}
+              title={album.name}
+              caption={album.artist}
+              cover={album.cover}
+              color={album.color}
+              key={album.name}
             />
           ))}
         </Grid>
@@ -565,17 +564,17 @@ export const ExampleMusicApp = () => (
 
               <Flex align="center" gap="3">
                 <img
-                  src={songs[9].cover}
+                  src={songsHistory[6].cover}
                   width="48"
                   height="48"
                   style={{ borderRadius: 'var(--radius-2)' }}
                 />
                 <Box>
                   <Text size="1" as="div" weight="medium">
-                    {songs[9].name}
+                    {songsHistory[6].name}
                   </Text>
                   <Text size="1" as="div" color="gray" mb="2">
-                    {songs[9].artist} – {songs[9].album}
+                    {songsHistory[6].artist} – {songsHistory[6].album}
                   </Text>
 
                   <Box
@@ -595,7 +594,7 @@ export const ExampleMusicApp = () => (
                     />
                     <Box position="absolute" top="0" right="0" style={{ marginTop: -28 }}>
                       <Text size="1" color="gray">
-                        0:58 / {songs[9].length}
+                        0:58 / {songsHistory[6].length}
                       </Text>
                     </Box>
                   </Box>
@@ -1005,7 +1004,7 @@ export const ExampleMusicApp = () => (
               <img
                 width="200"
                 height="200"
-                src="https://media.pitchfork.com/photos/59d6ab987855fa6c9a16f2f1/1:1/w_320,c_limit/the%20ooz_king%20krule.jpg"
+                src="https://workos.imgix.net/images/e35b46dc-4384-43d1-932c-24fa44e212cd.png?auto=format&fit=clip&q=80"
                 style={{
                   borderRadius: 'var(--radius-3)',
                   boxShadow: '0 8px 80px -24px hsl(205, 100%, 50%)',
@@ -1219,7 +1218,17 @@ export const ExampleMusicApp = () => (
   </Flex>
 );
 
-const AlbumCard = ({ album, artist, cover, color }: Partial<typeof songs[number]>) => (
+const AlbumCard = ({
+  title,
+  caption,
+  cover,
+  color,
+}: {
+  title: string;
+  caption: string;
+  cover: string;
+  color: string;
+}) => (
   <Hover.Root>
     <Box p="3" m="-3">
       <Box mb="2">
@@ -1258,23 +1267,24 @@ const AlbumCard = ({ album, artist, cover, color }: Partial<typeof songs[number]
 
       <Flex direction="column" position="relative" align="start">
         <Link size="2" weight="medium" color="gray" highContrast style={{ textDecoration: 'none' }}>
-          {album}
+          {title}
         </Link>
-        <Link size="2" color="gray">
-          {artist}
-        </Link>
+        <Text size="2" color="gray">
+          {caption}
+        </Text>
       </Flex>
     </Box>
   </Hover.Root>
 );
 
-const songs = [
+const songsQueue = [
   {
     name: 'The Less I Know the Better',
     artist: 'Tame Impala',
     album: 'Currents',
     length: '3:39',
-    cover: 'https://i.pinimg.com/originals/68/d1/26/68d126de72d2dead4920b9e09fa6af8b.jpg',
+    cover:
+      'https://workos.imgix.net/images/79645741-51e0-47fc-bb40-2fa66cf9f68e.png?auto=format&fit=clip&q=80&w=192',
     color: 'hsl(339, 80%, 60%)',
   },
   {
@@ -1282,7 +1292,8 @@ const songs = [
     artist: 'Villagers',
     album: 'Becoming a Jackal',
     length: '5:25',
-    cover: 'https://www.normanrecords.com/artwork/large/99/116349-villagers-becoming-a-jackal.jpg',
+    cover:
+      'https://workos.imgix.net/images/95ff9b99-36f3-46d8-a3fe-9387fd7c3c32.png?auto=format&fit=clip&q=80&w=192',
     color: 'hsl(356, 80%, 31%)',
   },
   {
@@ -1290,23 +1301,47 @@ const songs = [
     artist: 'Arlo Parks',
     album: 'Super Sad Generation',
     length: '3:50',
-    cover: 'https://images.genius.com/be8d58aa34e4c0acb093c4833101a773.1000x1000x1.png',
+    cover:
+      'https://workos.imgix.net/images/945c66a9-afd9-4b1c-8eb0-4ce3992731ca.png?auto=format&fit=clip&q=80&w=192',
     color: 'hsl(315, 90%, 70%, 0.2)',
   },
   {
-    name: 'Self',
-    artist: 'noname',
-    album: 'Room 25',
-    length: '1:35',
-    cover: 'https://f4.bcbits.com/img/a1836574169_10.jpg',
-    color: 'hsl(185, 60%, 43%)',
+    name: 'Do the Astral Plane',
+    artist: 'Flying Lotus',
+    album: 'Cosmogramma',
+    length: '3:58',
+    cover:
+      'https://workos.imgix.net/images/3d9075e4-c232-4fb5-a1a4-b0a33d669192.png?auto=format&fit=clip&q=80&w=192',
+    color: 'hsl(80, 9%, 66%)',
   },
   {
     name: 'Left Hand Free',
     artist: 'Alt-J',
     album: 'This Is All Yours',
     length: '2:54',
-    cover: 'https://i.scdn.co/image/ab67616d0000b27311b3df35e2e46d91f585afd9',
+    cover:
+      'https://workos.imgix.net/images/8d431b64-ebe8-41be-b986-2f59cb5c567d.png?auto=format&fit=clip&q=80&w=192',
+    color: 'hsl(330, 70%, 64%)',
+  },
+];
+
+const songsHistory = [
+  {
+    name: 'Sunday Rain',
+    artist: 'Foo Fighters',
+    album: 'Concrete and Gold',
+    length: '6:11',
+    cover:
+      'https://workos.imgix.net/images/28bf3f7c-4ad7-4bd9-9064-c63d2676c8dd.png?auto=format&fit=clip&q=80&w=192',
+    color: 'hsl(40, 30%, 55%)',
+  },
+  {
+    name: 'Left Hand Free',
+    artist: 'Alt-J',
+    album: 'This Is All Yours',
+    length: '2:54',
+    cover:
+      'https://workos.imgix.net/images/8d431b64-ebe8-41be-b986-2f59cb5c567d.png?auto=format&fit=clip&q=80&w=192',
     color: 'hsl(330, 70%, 64%)',
   },
   {
@@ -1314,7 +1349,8 @@ const songs = [
     artist: 'Nine Inch Nails',
     album: 'Broken',
     length: '4:45',
-    cover: 'https://i.scdn.co/image/ab67616d0000b273f1234cfed5f06fd58e76b06f',
+    cover:
+      'https://workos.imgix.net/images/5f495e55-4bac-4573-b97f-bac55d4f3a82.png?auto=format&fit=clip&q=80&w=192',
     color: 'hsl(30, 100%, 50%)',
   },
   {
@@ -1323,23 +1359,25 @@ const songs = [
     album: 'TA13OO',
     length: '3:31',
     cover:
-      'http://is5.mzstatic.com/image/thumb/Music115/v4/be/83/a0/be83a08f-171f-022c-d6f3-cbf5110492a5/source/1000x1000-999.jpg',
+      'https://workos.imgix.net/images/f1b1ff42-eae9-4fcd-9c7f-c3ed92594395.png?auto=format&fit=clip&q=80&w=192',
     color: 'hsl(0, 0%, 25%)',
   },
   {
-    name: 'Blaxploitation',
-    artist: 'noname',
-    album: 'Room 25',
-    length: '2:13',
-    cover: 'https://f4.bcbits.com/img/a1836574169_10.jpg',
-    color: 'hsl(185, 60%, 43%)',
+    name: 'Self Control',
+    artist: 'Frank Ocean',
+    album: 'Blond',
+    length: '4:10',
+    cover:
+      'https://workos.imgix.net/images/419f09bc-99ab-4eae-8e71-d33f0577bd47.png?auto=format&fit=clip&q=80&w=192',
+    color: 'hsl(80, 20%, 40%)',
   },
   {
     name: 'Trippy (feat. J. Cole)',
     artist: 'Anderson .Paak',
     album: 'Oxnard',
     length: '5:24',
-    cover: 'https://media.pitchfork.com/photos/5be9b09fa1b4df7cd2613d29/1:1/w_600/oxnard.jpg',
+    cover:
+      'https://workos.imgix.net/images/daab7042-222f-433f-abcb-15811b8a43da.png?auto=format&fit=clip&q=80&w=192',
     color: 'hsl(193, 15%, 45%)',
   },
   {
@@ -1348,7 +1386,7 @@ const songs = [
     album: 'The Idiot',
     length: '4:16',
     cover:
-      'https://fiu-original.b-cdn.net/fontsinuse.com/use-images/161/161513/161513.jpeg?filename=Iggy%20Pop%20%E2%80%93%20The%20Idiot.jpg',
+      'https://workos.imgix.net/images/85451af7-27bf-4bbb-88e7-088caf762ed5.png?auto=format&fit=clip&q=80&w=192',
     color: 'hsl(34, 7%, 45%)',
   },
   {
@@ -1356,7 +1394,8 @@ const songs = [
     artist: 'Alice in Chains',
     album: 'Alice in Chains',
     length: '5:28',
-    cover: 'https://e.snmc.io/i/1200/s/cf90f9e23c6b5bd2a18df298269028d4/1677357',
+    cover:
+      'https://workos.imgix.net/images/72edfcaf-2e5b-492c-bb5b-60a031f001c9.png?auto=format&fit=clip&q=80&w=192',
     color: 'hsl(289, 3%, 51%)',
   },
   {
@@ -1364,7 +1403,8 @@ const songs = [
     artist: 'Laura Marling',
     album: 'A Creature I Don’t Know',
     length: '5:08',
-    cover: 'https://media.pitchfork.com/photos/5929b0e5b1335d7bf169a1be/1:1/w_600/bae77272.jpg',
+    cover:
+      'https://workos.imgix.net/images/0cce32ae-6890-419e-b01c-2e89d36cb883.png?auto=format&fit=clip&q=80&w=192',
     color: 'hsl(40, 13%, 83%)',
   },
   {
@@ -1373,8 +1413,122 @@ const songs = [
     album: 'HEAVN',
     length: '4:23',
     cover:
-      'https://i.discogs.com/trqSgeVgjiKjDE2pJloIHKL5R6nEJkq8Q-yZQiCEL58/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEwOTMy/NzkyLTE1MDY3NTgw/MzItNzU3NS5qcGVn.jpeg',
+      'https://workos.imgix.net/images/e865c892-5cbe-4d1f-b4eb-e2bc301087f0.png?auto=format&fit=clip&q=80&w=192',
     color: 'hsl(32, 95%, 67%)',
+  },
+];
+
+const albumsFavorites = [
+  {
+    name: 'Blond',
+    artist: 'Frank Ocean',
+    cover:
+      'https://workos.imgix.net/images/419f09bc-99ab-4eae-8e71-d33f0577bd47.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(0, 0%, 50%, 0.2)',
+  },
+  {
+    name: 'Konoyo',
+    artist: 'Tim Hecker',
+    cover:
+      'https://workos.imgix.net/images/d2e1f2a4-5994-4f38-b3f4-0c78c946b616.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(210, 30%, 40%)',
+  },
+  {
+    name: 'Los Angeles',
+    artist: 'Flying Lotus',
+    cover:
+      'https://workos.imgix.net/images/29a1153b-e12a-45d8-95a7-812657566204.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(200, 30%, 20%, 0.2)',
+  },
+  {
+    name: 'The Fragile',
+    artist: 'Nine Inch Nails',
+    cover:
+      'https://workos.imgix.net/images/c33df9ee-0126-450b-ac7c-df1b76fc12da.png?auto=format&fit=clip&q=80',
+    color: 'hsl(15, 80%, 50%)',
+  },
+  {
+    name: 'Sketches of Spain',
+    artist: 'Miles Davis',
+    cover:
+      'https://workos.imgix.net/images/bc4dcddb-c350-413b-bc1f-38126cf9015b.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(35, 100%, 50%)',
+  },
+];
+
+const playlistsForYou = [
+  {
+    title: 'Rebellious 90’s and 00’s',
+    caption: 'Throwback to the teenage years',
+    cover:
+      'https://workos.imgix.net/images/5449710d-b6c6-467e-95cd-b1d016cac2ed.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(340, 70%, 50%)',
+  },
+  {
+    title: 'Soft Rock',
+    caption: 'Songs you can’t go wrong with',
+    cover:
+      'https://workos.imgix.net/images/9488cafc-5341-4164-a292-e34bf21dfbd5.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(235, 50%, 40%, 0.4)',
+  },
+  {
+    title: 'Trip-Hop Essentials',
+    caption: 'Dark and moody grooves',
+    cover:
+      'https://workos.imgix.net/images/dae75b0e-081b-43db-9984-b920de71e028.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(230, 100%, 50%, 0.3)',
+  },
+  {
+    title: 'Vintage Jazz',
+    caption: 'Travel through the times',
+    cover:
+      'https://workos.imgix.net/images/543ebff8-5f5c-4278-abb3-e1f17b723c30.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(260, 100%, 50%, 0.3)',
+  },
+  {
+    title: 'Funk Up',
+    caption: 'Irresistible beats',
+    cover:
+      'https://workos.imgix.net/images/01d61c66-7b4c-482d-b439-aa05f8845a55.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(260, 100%, 50%, 0.3)',
+  },
+];
+
+const albumsFriendsListen = [
+  {
+    name: 'Vulgar Display of Power',
+    artist: 'Pantera',
+    cover:
+      'https://workos.imgix.net/images/52719781-7582-49b3-9cd5-090acbab44ad.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(5, 20%, 70%)',
+  },
+  {
+    name: 'ONUKA',
+    artist: 'ONUKA',
+    cover:
+      'https://workos.imgix.net/images/21f983e3-2ed2-411c-b442-ddff52e4b5fd.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(0, 0%, 50%, 0.2)',
+  },
+  {
+    name: 'Consolation',
+    artist: 'Protomartyr',
+    cover:
+      'https://workos.imgix.net/images/ce6430c1-6375-4de5-b1ce-40d69872f622.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(50, 90%, 50%)',
+  },
+  {
+    name: 'uknowhatimsayin¿',
+    artist: 'Danny Brown',
+    cover:
+      'https://workos.imgix.net/images/3ea72e36-fcc5-4cc4-a378-444f48a5be5d.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(164, 85%, 50%)',
+  },
+  {
+    name: 'Floating Points',
+    artist: 'Crush',
+    cover:
+      'https://workos.imgix.net/images/1dfabeef-80f4-47b8-a8c4-7bec6c4b8b0d.png?auto=format&fit=clip&q=80&w=500',
+    color: 'hsl(210, 40%, 50%)',
   },
 ];
 
